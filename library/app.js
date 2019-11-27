b7a61d96965369d77c0d2c39b54a07d0d093a4bd
giselromina <giselromina@gmail.com>
2019-11-27 09:41:02 +0100

    Install Eslint

--- library/app.js
+++ library/app.js
@@ -9,8 +9,13 @@ var app = express();
 
 app.use(morgan('tiny'));
 
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
 app.get('/',function(req,res){
-  res.sendfile(path.join(__dirname,'views/index.html'));
+  res.sendFile(path.join(__dirname, '/views/', '/index.html')); 
 
 });
 
