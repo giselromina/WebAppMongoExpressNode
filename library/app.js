e63efbb680fd3f0fe8feeeb369fbfa2d8aa7c432
giselromina <giselromina@gmail.com>
2019-11-29 19:01:01 +0100

    change and make routes

--- library/app.js
+++ library/app.js
@@ -7,8 +7,10 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(morgan('tiny'));
+const bookRouter = express.Router();
+const authorRouter = express.Router();
 
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -16,8 +18,38 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 //app.set('view engine', 'pug');
+
+bookRouter.route('/')
+.get((req,res)=>{
+  res.send('Hello Books');
+});
+bookRouter.route('/single')
+.get((req,res)=>{
+  res.send('hello single books');
+});
+
+authorRouter.route('/').
+get((req,res)=>{
+res.send('Hello Authors');
+});
+
+authorRouter.route('/singleAuthor').
+get((req,res)=>{
+  res.send('Hello single Author');
+});
+
+app.use('/books', bookRouter);
+app.use('/authors',authorRouter);
 app.get('/', (req, res) => {
-  res.render('index', { List: ['a', 'b', 'c'], title: " Made for Gisel " });
+  res.render('index', 
+  {
+     navs: [
+            {link:'/books', title:'Books'},
+            {link:'/authors',title:'Authors'}
+     ],
+     title: 'Made for Gisel'  
+  }
+     );
 });
 app.listen(port, () => {
   debug(`listening on port ${chalk.green(port)}`);
