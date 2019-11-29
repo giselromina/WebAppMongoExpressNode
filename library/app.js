160f1fe420e8c8c5d362103ad94b114f945fd26d
giselromina <giselromina@gmail.com>
2019-11-29 08:57:47 +0100

    ejs for create HTML marks with plain js

--- library/app.js
+++ library/app.js
@@ -13,10 +13,11 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
-app.set('views','./src/views');
-app.set('view engine','pug');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+//app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-  res.render('index', { List: ['a','b','c'] });
+  res.render('index', { List: ['a', 'b', 'c'], title: " Made for Gisel " });
 });
 app.listen(port, () => {
   debug(`listening on port ${chalk.green(port)}`);
